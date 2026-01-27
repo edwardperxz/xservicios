0e6cd1edcffc6ec08e087f65451e6a3f9efeae46
fredyallard01 <fredy.allard@utp.ac.pa>
2026-01-27 15:31:20 -0500

    Se rehizo el codigo de la liniea XservUsuario.php en Model/Entity

--- src/Model/Entity/XservUsuario.php
+++ src/Model/Entity/XservUsuario.php
@@ -4,46 +4,31 @@ declare(strict_types=1);
 namespace App\Model\Entity;
 
 use Cake\ORM\Entity;
+use Authentication\PasswordHasher\DefaultPasswordHasher;
 
-/**
- * XservUsuario Entity
- *
- * @property int $id
- * @property string $name
- * @property string $email
- * @property string $password
- * @property string $rol
- * @property bool $activo
- * @property \Cake\I18n\DateTime $created
- * @property \Cake\I18n\DateTime $modified
- */
 class XservUsuario extends Entity
 {
-    /**
-     * Fields that can be mass assigned using newEntity() or patchEntity().
-     *
-     * Note that when '*' is set to true, this allows all unspecified fields to
-     * be mass assigned. For security purposes, it is advised to set '*' to false
-     * (or remove it), and explicitly make individual fields accessible as needed.
-     *
-     * @var array<string, bool>
-     */
     protected array $_accessible = [
-        'name' => true,
-        'email' => true,
-        'password' => true,
-        'rol' => true,
-        'activo' => true,
-        'created' => true,
-        'modified' => true,
+        'username'   => true,
+        'password'   => true,
+        'rol'        => true,
+        'estado'     => true,
+        'created_at' => true,
+        'updated_at' => true,
     ];
 
-    /**
-     * Fields that are excluded from JSON versions of the entity.
-     *
-     * @var array<string>
-     */
     protected array $_hidden = [
         'password',
     ];
+
+    /**
+     * Hash automático del password
+     */
+    protected function _setPassword(?string $password): ?string
+    {
+        if (strlen((string)$password) > 0) {
+            return (new DefaultPasswordHasher())->hash($password);
+        }
+        return null;
+    }
 }

9a27928d9d9bfdd10ceff3ba1ce71d175675125b
fredyallard01 <fredy.allard@utp.ac.pa>
2026-02-04 03:26:39 -0500

    Se hicieron cambios en /routes.php: El root del sitio web te redirige a la landing page, linea de codigos: 14 y 15

--- config/routes.php
+++ config/routes.php
@@ -10,7 +10,9 @@ return function (RouteBuilder $routes): void {
     $routes->scope('/', function (RouteBuilder $builder): void {
 
         // Home real de Xservicios
-        $builder->connect('/', ['controller' => 'Pages', 'action' => 'home']);
+        //$builder->connect('/', ['controller' => 'Pages', 'action' => 'home']);
+        $builder->redirect('/', '/home');
+        $builder->connect('/home', ['controller' => 'Home', 'action' => 'index',]);
 
         // Páginas estáticas (si las necesitas después)
         $builder->connect('/pages/*', 'Pages::display');

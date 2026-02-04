5a64ebb257a94a1327589284f7653c88a87374dd
fredyallard01 <fredy.allard@utp.ac.pa>
2026-02-04 02:59:46 -0500

    Se completo la ruta /home para que sea la Landing page del sitio web

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,4 +1,6 @@
 <?php
+declare(strict_types=1);
+
 namespace App\Controller;
 
 class HomeController extends AppController
@@ -12,7 +14,26 @@ class HomeController extends AppController
 
     public function index()
     {
+        $this->Authorization->skipAuthorization();
         $user = $this->request->getAttribute('identity');
-        $rol = $user->rol;
+        $this->viewBuilder()->setLayout('xservicios');
+
+        // Si hay sesión, luego podrás redirigir por rol
+        if ($user) {
+            $rol = $user->rol;
+
+            // (opcional por ahora, lo dejamos preparado)
+            // switch ($rol) {
+            //     case 'admin':
+            //         return $this->redirect(['controller' => 'Dashboard', 'action' => 'admin']);
+            //     case 'operador':
+            //         return $this->redirect(['controller' => 'Dashboard', 'action' => 'operador']);
+            //     case 'chofer':
+            //         return $this->redirect(['controller' => 'Dashboard', 'action' => 'chofer']);
+            // }
+        }
+
+        // Usuario NO logueado → home público (root)
+        $this->render('/Pages/home');
     }
 }
